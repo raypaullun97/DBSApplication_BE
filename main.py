7b68f3814f0627740553b947f0bc10cd81b44e1a
shafiq98 <shafiq_riser@outlook.com>
2022-12-03 10:29:49 +0800

    updated main

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from core import app
 import logging
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
 
-
-
 if (__name__ == "__main__"):
 
     logging.debug("Hello World")
