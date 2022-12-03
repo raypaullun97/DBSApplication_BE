d49ab6e3dee894c73f5a319a6dafca959a65088d
DK36369 <71749676+DK36369@users.noreply.github.com>
2022-12-03 12:05:45 +0800

    Commit For Deleting Future Transection

---
+++ core/future_transection.py
@@ -0,0 +1,25 @@
+import pymysql
+from app import app
+from db_config import mysql
+from flask import jsonify
+from flask import flash, request
+from werkzeug import generate_password_hash, check_password_hash
+
+@app.route('/ft/<int:id>')
+def deleteFutureTrans(id):
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("DELETE scheduledtransactions WHERE AccountID={0}".format(id))
+		rows = cursor.fetchall()
+		resp = jsonify(rows)
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+if __name__ == "__main__":
+    app.run()
