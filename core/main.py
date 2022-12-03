15d4b0eaa49a7cb381ece86933ba49a9d37d206c
raypaullun <limweilun1997@gmail.com>
2022-12-03 12:01:32 +0800

    completed insert transaction

--- core/main.py
+++ core/main.py
@@ -22,6 +22,33 @@ def transaction(a_id):
 		cursor.close() 
 		conn.close()
 
+@app.route('/addtransaction', methods=['POST'])
+def addtransaction():
+	try:
+		_json = request.json
+		_TransactionAmount = _json['TransactionAmount']
+		_Comment = _json['Comment']
+		_AccountID = _json['AccountID']
+		_ReceivingAccountID = _json['ReceivingAccountID']
+		_Date = _json['Date']
+		# validate the received values
+		if _TransactionAmount and _Comment and _AccountID and _ReceivingAccountID and _Date and request.method == 'POST':
+			sql = "INSERT INTO scheduledtransactions(AccountID, ReceivingAccountID, Date, TransactionAmount, Comment) VALUES(%s, %s, %s, %s, %s)"
+			data = (_AccountID, _ReceivingAccountID, _Date, _TransactionAmount, _Comment,)
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.execute(sql, data)
+			conn.commit()
+			resp = jsonify('Transaction added successfully!')
+			resp.status_code = 200
+			return resp
+		else:
+			return not_found()
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
 
 		
 
