beb5ff10c6007904e72d7371381b458fbf36b639
jowannyeak <50290926+jowannyeak@users.noreply.github.com>
2022-12-03 11:57:49 +0800

    Update main.py with user route

--- core/main.py
+++ core/main.py
@@ -5,6 +5,37 @@ from flask import jsonify
 from flask import flash, request
 
 		
+@app.route('/account/login', methods=['POST'])
+def login():
+    try:
+        _json = request.get_json()
+        _username = _json['credentials']['username']
+        _password = _json['credentials']['password']
+        if _username and _password and request.method == 'POST':
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            cursor.execute(f"SELECT * FROM User WHERE Username=%s and Password=%s",( _username, _password,))
+            row = cursor.fetchone()
+            if row != None:
+                resp = jsonify({
+                    "code": 200,
+                    "data": "Login successful."
+                })
+            else:
+                return jsonify({
+                    "code": 401,
+                    "data": "Either your username or password is wrong."
+                })
+            resp.status_code = 200
+            return resp
+        else: 
+            return not_found()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close() 
+        conn.close()
+
 @app.route('/transaction/<int:a_id>')
 def transaction(a_id):
 	try:
