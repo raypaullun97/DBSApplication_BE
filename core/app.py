4851506cb55cb90fa40cff77a743d9018f80d243
shafiq98 <shafiq_riser@outlook.com>
2022-12-03 11:30:34 +0800

    changed account_details method

--- core/app.py
+++ core/app.py
@@ -1,19 +1,8 @@
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import urllib
 import logging
+import os
 
+from flask import Flask
 app = Flask(__name__)  # Standard Flask app
-
-params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};"
-                                 "SERVER=(local);"
-                                 "DATABASE=test2;"
-                                 "Trusted_Connection=yes")
-
-app.config['SQLALCHEMY_DATABASE_URI'] = ("mssql+pyodbc:///?odbc_connect={}".format(params))
-db = SQLAlchemy(app)
-logging.debug("DB setup complete")
 
 @app.route("/")
 def hello():
@@ -26,8 +15,9 @@ def hello():
 
 @app.route("/details/<string:name>", methods=['POST'])
 def get_account_details(name):
-    return {"message" : "get_account_details"}
-
+    logging.debug("GET '/details/name'")
+    # db.session.close()
+    return {"message": "Hello world"}
 
 def runner():
     app.run(host='0.0.0.0', port=os.environ.get('listenport', 8080))
